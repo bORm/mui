4607cfdee91c9b4ab06773e70c77acfc08769f75
Олег Зайцев <olegzaitsevway@gmail.com>
2016-09-20 17:35:50 +0300

    component Tooltip

---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,17 @@
+import React, {Component, PropTypes} from 'react'
+
+class Tooltip extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Tooltip
