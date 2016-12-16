1ef830f7e2b7951574ee8e2ee4829fdcfaeca1c1
Олег Зайцев <olegzaitsevway@gmail.com>
2016-12-16 17:47:36 +0200

    add Radio Button html & styles in near future it will be improved

---
+++ src/components/RadioButton/RadioButton.js
@@ -0,0 +1,23 @@
+import React, {Component, PropTypes} from 'react'
+
+class RadioButton extends Component {
+  static propTypes = {};
+
+  static defaultProps = {};
+
+  render() {
+    return (
+      <div>
+        <div className="radio-btn">
+          <label className="control control--radio">
+            <input type="radio" />
+            <span>Some text</span>
+            <div className="control__indicator"></div>
+          </label>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RadioButton
